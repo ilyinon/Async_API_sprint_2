c0214cce63677bd2cd324516c0ca22e7a7561899
Oleg <ilyinon@mail.ru>
2024-09-13 20:46:53 +0300

    SOLID ES добавить классы от которых можно наследовать нужные методы (#14)
    * добавить классы от которых можно наследовать нужные методы
    
    * исправлено замечания из ревью

--- app/services/genre.py
+++ app/services/genre.py
@@ -10,31 +10,14 @@ from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
 from models.genre import Genre
 from redis.asyncio import Redis
+from services.base import BaseServiceElastic, BaseServiceRedis
 
 logger = logging.getLogger(__name__)
 
 
-class GenreService:
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
-        self.redis = redis
-        self.elastic = elastic
-
-    def _generate_cache_key(self, genre_id):
-        return f"genre:{genre_id}"
-
-    async def get_by_id(self, genre_id: UUID) -> Genre | None:
-        cache_key = self._generate_cache_key(genre_id)
-        cached_data = await self.redis.get(cache_key)
-        if cached_data:
-            return Genre.parse_raw(cached_data)
-
-        genre = await self._get_genre_from_elastic(genre_id)
-        if genre:
-            await self.redis.set(
-                cache_key, genre.json(), settings.genre_cache_expire_in_seconds
-            )
-
-        return genre
+class GenreService(BaseServiceRedis, BaseServiceElastic):
+    async def get_by_id(self, film_id):
+        return await self._get_by_id(film_id, Genre)
 
     async def get_list(self, page_number, page_size):
         cache_key = f"genres_list:{page_size}:{page_number}"
@@ -61,7 +44,7 @@ class GenreService:
 
         return genres
 
-    async def _get_genre_from_elastic(self, genre_id: UUID) -> Genre | None:
+    async def _get_object_from_elastic(self, genre_id: UUID) -> Genre | None:
         try:
             doc = await self.elastic.get(index="genres", id=genre_id)
         except NotFoundError:
@@ -70,19 +53,6 @@ class GenreService:
         answer["id"] = doc["_source"]["id"]
         answer["name"] = doc["_source"]["name"]
         return Genre(**answer)
-
-    async def _genre_from_cache(self, genre_id: UUID) -> Genre | None:
-        data = await self.redis.get(str(genre_id))
-        if not data:
-            return None
-        logger.info(f"Retrieved genre {genre_id} from cache")
-        genre = Genre.parse_raw(data)
-        return genre
-
-    async def _put_genre_to_cache(self, genre: Genre):
-        await self.redis.set(
-            str(genre.id), genre.json(), settings.genre_cache_expire_in_seconds
-        )
 
 
 @lru_cache()
