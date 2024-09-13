1184f69d790446b4a831557259677e2e496f8e46
Дмитрий Шабанов <shabanov010@yandex.ru>
2024-09-13 20:46:13 +0300

    Сдвинул тесты с мертвой точки

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -3,13 +3,13 @@ from pydantic_settings import BaseSettings
 
 
 class TestSettings(BaseSettings):
-    es_host: str = Field('http://127.0.0.1:9200')
+    es_host: str = Field('http://elastic:9200')
     es_index: str = 'movies'
     # es_id_field: str
     es_index_mapping: dict
 
-    redis_host: str = Field('redis://127.0.0.1:6379')
-    service_url: str = Field('http://127.0.0.1:80')
+    redis_host: str = Field('redis://redis:6379')
+    service_url: str = Field('http://nginx:80')
 
 
 es_index_mapping = {
