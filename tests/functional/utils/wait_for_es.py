290c1e7240337688b0e9a352c0121cd339b5b27a
Oleg Ilin <ilyinon@mail.ru>
2024-09-17 23:59:35 +0300

    добавить backoff

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,17 +1,19 @@
-import sys
-import time
-
 from elasticsearch import Elasticsearch
+from settings import settings
+from utils.backoff import backoff
+from utils.logger import logger
 
-from tests.functional.settings import settings
 
-sys.path.append("/opt/tests")
-
-
-# TODO: @backoff()
-if __name__ == "__main__":
+@backoff()
+def wait_for_es():
     es_client = Elasticsearch(hosts=settings.elastic_dsn)
-    while True:
-        if es_client.ping():
-            break
-        time.sleep(1)
+    ping = es_client.ping()
+    logger.info(ping)
+    if ping:
+        return ping
+    raise Exception
+
+
+if __name__ == "__main__":
+
+    wait_for_es()
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,16 +1,18 @@
-import time
-
 from redis.client import Redis
+from settings import settings
+from utils.backoff import backoff
+from utils.logger import logger
 
-from tests.functional.settings import settings
 
-# sys.path.append('/opt/tests/functional')
-
-
-# TODO: @backoff()
-if __name__ == "__main__":
+@backoff()
+def wait_for_redis():
     redis_client = Redis(host=settings.redis_host, port=settings.redis_port, db=0)
-    while True:
-        if redis_client.ping():
-            break
-        time.sleep(1)
+    ping = redis_client.ping()
+    logger.info(ping)
+    if ping:
+        return ping
+    raise Exception
+
+
+if __name__ == "__main__":
+    wait_for_redis()
