64244b8da178c7d415377b03d9f3a5f15bda68eb
Tamara Savadyan <attamaras@Tamaras-MacBook-Pro.local>
2024-09-19 13:38:06 +0300

    первоначальное изменение работы с эластиком, нигде не используемая

--- app/services/base.py
+++ app/services/base.py
@@ -1,4 +1,6 @@
+from abc import ABC, abstractmethod
 import logging
+from typing import Any
 from uuid import UUID
 
 from core.config import settings
@@ -6,6 +8,71 @@ from elasticsearch import AsyncElasticsearch
 from redis.asyncio import Redis
 
 logger = logging.getLogger(__name__)
+
+class AsyncSearchEngine(ABC):
+    @abstractmethod
+    async def get_by_id(self, index: str, _id: str) -> Any | None:
+        pass
+
+    @abstractmethod
+    async def _get_by_ids(self, index: str, ids: list[str]) -> list[Any] | None:
+        pass
+
+    @abstractmethod
+    async def search(self, index: str, query: dict) -> list[Any]:
+        pass
+
+
+class ElasticAsyncSearchEngine(AsyncSearchEngine):
+    def __init__(self, client: AsyncElasticsearch):
+        self.client = client
+
+    async def get_by_id(self, index: str, _id: str) -> Any | None:
+        try:
+            result = await self.client.get(index=index, id=_id)
+            return result["_source"]
+        except Exception:
+            return None
+
+    async def get_by_ids(self, index: str, ids: list[str]) -> list[Any] | None:
+        try:
+            result = await self.client.mget(index=index, body={"ids": ids})
+            return [doc["_source"] for doc in result["docs"] if doc["found"]]
+        except Exception:
+            return []
+
+    async def search(self, index: str, query: dict) -> list[Any]:
+        try:
+            result = await self.client.search(index=index, body=query)
+            return [hit["_source"] for hit in result["hits"]["hits"]]
+        except Exception:
+            return []
+
+class BaseService234(ABC):
+    def __init__(self, search_engine: AsyncSearchEngine):
+        self.search_engine = search_engine
+
+    async def get_by_id(self, index: str, obj_id: str) -> Any | None:
+        obj = await self.search_engine.get_by_id(index=index, _id=obj_id)
+        return obj
+
+    async def get_by_ids(self, index: str, obj_ids: list[str]) -> list[Any]:
+        objs = await self.search_engine.get_by_ids(index=index, ids=obj_ids)
+        return objs
+
+    async def search(self, index: str, query: dict) -> list[Any]:
+        objs = await self.search_engine.search(index=index, query=query)
+        return objs
+
+
+
+# class BaseService(abc.ABC):
+#     def __init__(self, search_engine: AsyncSearchEngine):
+#         self.search_engine = search_engine
+
+#     async def get_by_id(self, obj_id: str) -> Optional[BaseServiceModelChild]:
+#         obj = await self.search_engine.get_by_id(obj_id)
+#         return obj
 
 
 class BaseService:
