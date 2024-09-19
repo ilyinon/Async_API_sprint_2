9bd5c7d8312b69ad41342e9f494f27811cedf03d
Tamara Savadyan <attamaras@Tamaras-MacBook-Pro.local>
2024-09-19 17:02:36 +0300

    исправлена логика работы с эластиком и редисом, обновлен  genre.py

--- app/services/genre.py
+++ app/services/genre.py
@@ -6,56 +6,66 @@ from uuid import UUID
 from core.config import settings
 from db.elastic import get_elastic
 from db.redis import get_redis
-from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 from models.genre import Genre
 from redis.asyncio import Redis
-from services.base import BaseServiceElastic, BaseServiceRedis
+
+from services.base import BaseServiceRedis
+from services.cache import BaseCache, RedisCacheEngine
+from services.search import BaseSearch, ElasticAsyncSearchEngine
 
 logger = logging.getLogger(__name__)
 
 
-class GenreService(BaseServiceRedis, BaseServiceElastic):
-    async def get_by_id(self, film_id):
-        return await self._get_by_id(film_id, Genre)
+class GenreService(BaseServiceRedis):
+    def __init__(self, cache_engine: BaseCache, search_engine: BaseSearch):
+        self.search_engine = search_engine
+        self.cache_engine = cache_engine
 
-    async def get_list(self, page_number, page_size):
-        cache_key = f"genres_list:{page_size}:{page_number}"
-        cached_data = await self.redis.get(cache_key)
-        offset = (page_number - 1) * page_size
-        if cached_data:
-            return [Genre.parse_raw(genre) for genre in json.loads(cached_data)]
+    async def get_by_id(self, genre_id: UUID) -> Genre | None:
+        genre = await self.cache_engine.get_by_id('genre', genre_id, Genre)
 
-        try:
-            genres_list = await self.elastic.search(
-                index=settings.genres_index,
-                from_=offset,
-                size=page_size,
-                query={"match_all": {}},
+        if not genre:
+            genre_data = await self.search_engine.get_by_id(
+                settings.genres_index, genre_id
             )
-        except NotFoundError:
-            return None
 
-        genres = [
-            Genre(**get_genre["_source"]) for get_genre in genres_list["hits"]["hits"]
-        ]
-        await self.redis.set(
-            cache_key,
-            json.dumps([genre.json() for genre in genres]),
-            settings.genre_cache_expire_in_seconds,
+            if not genre_data:
+                return None
+
+            genre = Genre(**genre_data)
+
+            await self.cache_engine.put_by_id('genre', genre, settings.genre_cache_expire_in_seconds)
+
+        logger.info(f"Retrieved genre: {genre}")
+        return genre
+
+    async def get_list(self, page_number: int, page_size: int) -> list[Genre] | None:
+        cache_key_args = ("genres_list", page_size, page_number)
+        
+        cached_data = await self.cache_engine.get_by_key(*cache_key_args, Object=Genre)
+
+        if cached_data:
+            return cached_data
+
+        offset = (page_number - 1) * page_size
+
+        genres_list = await self.search_engine.search(
+            index=settings.genres_index,
+            query={"match_all": {}},
+            from_=offset,
+            size=page_size,
         )
 
-        return genres
-
-    async def _get_object_from_elastic(self, genre_id: UUID) -> Genre | None:
-        try:
-            doc = await self.elastic.get(index=settings.genres_index, id=genre_id)
-        except NotFoundError:
+        if not genres_list:
             return None
-        answer = {}
-        answer["id"] = doc["_source"]["id"]
-        answer["name"] = doc["_source"]["name"]
-        return Genre(**answer)
+
+        genres = [Genre(**genre) for genre in genres_list]
+
+        await self.cache_engine.put_by_key(genres, settings.genre_cache_expire_in_seconds, *cache_key_args)
+
+        return genres
 
 
 @lru_cache()
@@ -63,4 +73,11 @@ def get_genre_service(
     redis: Redis = Depends(get_redis),
     elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> GenreService:
-    return GenreService(redis, elastic)
+
+    redis_cache_engine = RedisCacheEngine(redis)
+    cache_engine = BaseCache(redis_cache_engine)
+
+    elastic_search_engine = ElasticAsyncSearchEngine(elastic)
+    search_engine = BaseSearch(search_engine=elastic_search_engine)
+
+    return GenreService(cache_engine, search_engine)
